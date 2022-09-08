a518188dee129882595fa23da72ddda95ea42a09
Ruxin Qu <rosheenqu@gmail.com>
2022-09-08 16:10:22 -0700

    the background color of each time block changes based on the current time. past:gray;current: red; furture: green;

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,34 @@
-const DateTime = luxon.DateTime;
-const { Info } = luxon;
-let today = DateTime.now();
+/* Description: This script.js file gets the current date, weekday and hour of today. The current date and weekday is shown on the top of the
+page. If the hour on the scheduler is past, the background  of the input area will be gray. If it's the current hour, the background will be red. If the hour on
+the scheduler is in the future, the background will be green*/ 
 
+$(document).ready(() => {
+    const DateTime = luxon.DateTime;
+    const { Info } = luxon;
+    let today = DateTime.now();
 
-const renderDate = () => {  
-    let weekNum = today.weekday;
-    $('#currentDay').text(`${Info.weekdays('long')[weekNum]},  ${today.toFormat('MMM dd, yyyy')}`)
-}
-
-const getCurrentHour = () => {
-    let currentHour = today.hour
-    if(currentHour > 12){
-        currentHour -= 12;
-        
+    // get the weekday of today and the date 
+    function renderDate() {
+        let weekNum = today.weekday;
+        $('#currentDay').text(`${Info.weekdays('long')[weekNum]},  ${today.toFormat('MMM dd, yyyy')}`)
     }
 
+    // run a .each on each textarea input and compare the currentHour with the id attribute inside textarea
+    function getCurrentHour() {
+        let currentHour = today.hour;
 
+        $('textarea').each(function () {
+            let displayTime = parseInt($(this).attr('id'))
+            if (currentHour > displayTime) {
+                $(this).addClass('past')
+            } else if (currentHour == displayTime) {
+                $(this).addClass('current')
+            } else {
+                $(this).addClass('future')
+            }
+        })
+    }
 
-
-
-}
-renderDate();
-console.log(getCurrentHour())
+    renderDate();
+    getCurrentHour();
+})
