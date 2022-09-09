d1a7c1ece54647c6904bca3c8a7a6aed2de2734d
Ruxin Qu <rosheenqu@gmail.com>
2022-09-08 17:50:16 -0700

    enable to save local storage

--- assets/userinput.js
+++ assets/userinput.js
@@ -1,1 +1,20 @@
 /* Description: This userinput.js file handles the user input and local storage */
+$(document).ready(() => {
+
+    const saveToDo = (event) => {
+        event.preventDefault();
+        const toDo = $(event.currentTarget).siblings('textarea').val()
+        const toDoTime = $(event.currentTarget).siblings('.hour').text()
+        localStorage.setItem(toDoTime, JSON.stringify(toDo));
+    }
+
+    const renderToDo = () => {
+        $('.hour').each(function () {
+            const textToRender = JSON.parse(localStorage.getItem($(this).text()));
+            $(this).siblings('textarea').val(textToRender);
+        })
+    }
+
+    $('.btn').on('click', saveToDo)
+    renderToDo();
+})
